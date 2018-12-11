538a1c82b8846bc9b34c6d3e9d0f6e0d7bfd2a0b
Azer Abdullaev <azer.abdullaev@protonmail.ch>
2018-12-11 12:38:53 +0100

    Updated default values

--- index.php
+++ index.php
@@ -13,23 +13,23 @@
             <form action="patchwork.php" method="GET">
                 <p>
                     <label for="user">Username</label>
-                    <input type="text" name="user" id="user" value="Dinduks" autofocus />
+                    <input type="text" name="user" id="user" value="like-all" autofocus />
                 </p>
                 <p>
                     <label for="period">Period</label>
                     <select name="period" id="period">
-                        <option value="overall">Overall</option>
-                        <option value="7day">7 days</option>
-                        <option value="3month">3 months</option>
-                        <option value="6month">6 months</option>
                         <option value="12month">One year</option>
+                        <option value="6month">6 months</option>
+                        <option value="3month">3 months</option>
+                        <option value="7day">7 days</option>
+                        <option value="overall">Overall</option>
                     </select>
                 </p>
                 <p>
                     <label for="rows">Nr. of rows</label>
                     <select name="rows" id="rows">
                         <?php for ($i=1; $i<=20; $i++) : ?>
-                        <option value="<?php echo $i; ?>" <?php echo ($i==5) ? "selected='selected'" : ""; ?>>
+                        <option value="<?php echo $i; ?>" <?php echo ($i==3) ? "selected='selected'" : ""; ?>>
                             <?php echo $i; ?>
                         </option>
                         <?php endfor; ?>
@@ -39,7 +39,7 @@
                     <label for="cols">Nr. of columns</label>
                     <select name="cols" id="cols">
                         <?php for ($i=1; $i<=20; $i++) : ?>
-                        <option value="<?php echo $i; ?>" <?php echo ($i==2) ? "selected='selected'" : ""; ?>>
+                        <option value="<?php echo $i; ?>" <?php echo ($i==3) ? "selected='selected'" : ""; ?>>
                             <?php echo $i; ?>
                         </option>
                         <?php endfor; ?>
@@ -47,7 +47,7 @@
                 </p>
                 <p>
                     <label for="imageSize">Images size</label>
-                    <input type="text" value="150" name="imageSize" id="imagesSize" /> px
+                    <input type="text" value="360" name="imageSize" id="imagesSize" /> px
                 </p>
                 <p>
                     <input type="submit" id="submit" value="Generate!" />
